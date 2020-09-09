39f31762b5c21426bbe65e11547e0c2aa24b2efd
Staci Amburgey <staci.m.amburgey@gmail.com>
2020-09-09 10:19:40 -0700

    Update to simulation model

--- SCRSimulation.R
+++ SCRSimulation.R
@@ -18,17 +18,19 @@ Sys.setenv(BINPREF = "C:/Rtools/mingw_$(WIN)/bin/")
 nc <- 3; nAdapt=500; nb <- 1000; ni <- 5000+nb; nt <- 1
 
 ## Define study area grid (random example currently)
-locs <- as.matrix(secr::make.grid(nx = 20, ny = 20, spacex = 8, spacey = 8))
+locs <- as.matrix(secr::make.grid(nx = 10, ny = 10, spacex = 8, spacey = 8))
 
 ## Which parts of grid have traps
 set.seed(922020)
-a=sample(200, 15)
-Yscr=locs[a,]
-ntraps <- nrow(Yscr)
+a=sample(100, 15)
+X=locs[a,]
+J <- nrow(X)
+# Yscr <- locs
+# ntraps <- nrow(locs)
 
 ## Define state-space of point process. (i.e., where animals live).
 ## "delta" just adds a fixed buffer to the outer extent of the traps.
-delta <- 20  ## will need to play with this to figure out delta where N doesn't change substantially
+delta <- 20  ## will need to play with this
 Xl<-min(locs[,1]) - delta
 Xu<-max(locs[,1]) + delta
 Yl<-min(locs[,2]) - delta
@@ -47,110 +49,116 @@ N <- round(A*0.0023)
 ## Number of nights trapping
 K <- 20
 
-## Simulate activity centers
-sx<-runif(N,Xl,Xu)
-sy<-runif(N,Yl,Yu)
-S<-cbind(sx,sy) 
-
-## Distance between each individual AC and each each trap
-## Function to calculate distance between two sets of (x,y) locations
-e2dist <- function(A, B)  {
-  xdif <- outer(A[, 1], B[, 1], "-")
-  ydif <- outer(A[, 2], B[, 2], "-")
-  sqrt(xdif^2 + ydif^2)
-}
-
-D <- e2dist(S,Yscr)
-muy <- lam0*exp(-(D*D)/(2*sigma^2))
+nsim <- 1
 
 
+for(iter in 1:nsim){
+  print(iter)
 
-
-
-
-## Simulate observations of individuals by trap
-Y<-matrix(NA,nrow=N,ncol=ntraps)
-for(i in 1:nrow(Y)){
-  Y[i,]<-rpois(ntraps,K*muy[i,])
-}
-datnam<-paste('Dat_', iter, '.R', sep='')
-dput(Y, datnam)
-
-## NOTE NOTE NOTE NOTE
-## Y is a matrix of encounter frequencies of EACH individual in EACH trap
-## As simulated here it includes the "all 0" observations.  We want
-## to delete those to mimic real data.
-
-Yscr=Y[,a]
-totalcaps<-apply(Yscr,1,sum)
-Yscr<-Yscr[totalcaps>0,]
-
-## Data augmentation
-M <- 200
-y <- rbind(y,matrix(0,nrow=M-nind,ncol=ncol(y)))
-z <- c(rep(1,nind),rep(0,M-nind))
-
-## Starting values for s
-sst <- cbind(runif(M,xlim[1],xlim[2]),runif(M,ylim[1],ylim[2]))
-for(i in 1:nind){
-  sst[i,1] <- mean( X[y[i,]>0,1] )
-  sst[i,2] <- mean( X[y[i,]>0,2] )
-}
-
-
-## NIMBLE model is nearly identical to BUGS
-code <- nimbleCode({
-  alpha0 ~ dnorm(0,.1)
-  logit(p0) <- alpha0
-  alpha1 ~ dnorm(0,.1)
-  sigma <- sqrt(1/(2*alpha1))
-  psi ~ dunif(0,1)
+  ## Simulate activity centers
+  sx<-runif(N,Xl,Xu)
+  sy<-runif(N,Yl,Yu)
+  S<-cbind(sx,sy) 
   
-  for(i in 1:M){
-    z[i] ~ dbern(psi)
-    s[i,1] ~ dunif(xlim[1],xlim[2])
-    s[i,2] ~ dunif(ylim[1],ylim[2])
-    d[i,1:J] <- pow(pow(s[i,1]-X[1:J,1],2) + pow(s[i,2]-X[1:J,2],2),0.5) #accepts vector assignments like R
-    p[i,1:J] <- z[i]*p0*exp(- alpha1*d[i,1:J]*d[i,1:J])
-    for(j in 1:J){
-      y[i,j] ~ dbin(p[i,j],K)
-    }#J
-  }#i
-  N <- sum(z[1:M]) #Must specify dimensions in NIMBLE
-  D <- N/64
-})#code
+  ## Distance between each individual AC and each each trap
+  ## Function to calculate distance between two sets of (x,y) locations
+  e2dist <- function(A, B)  {
+    xdif <- outer(A[, 1], B[, 1], "-")
+    ydif <- outer(A[, 2], B[, 2], "-")
+    sqrt(xdif^2 + ydif^2)
+  }
+  
+  ####### Modify this section for each monitoring method to generate observations #######
+  D <- e2dist(S,X)
+  muy <- lam0*exp(-(D*D)/(2*sigma^2))
+  
+  ## Simulate observations of individuals by trap
+  Y<-matrix(NA,nrow=N,ncol=J)
+  for(i in 1:nrow(Y)){
+    Y[i,]<-rpois(J,K*muy[i,])
+  }
+  
+  ##########
+  
+  # ## If needing other dimension later on
+  # Y<-array(NA,dim=c(N,ntraps,K))
+  # for(i in 1:nrow(Y)){
+  #   for(j in 1:ntraps){
+  #     Y[i,j,1:K]<-rpois(K,1,K*muy[i,j])  ## will require expanding muy[i,j] and then will need to also sum after this
+  #   }
+  # }
+  
+  datnam<-paste('/SimDat/Dat_', iter, '.R', sep='')
+  dput(Y, datnam)
+  
+  ## Data augmentation for when N is unknown
+  M <- round(N+(N*0.5))
+  y <- rbind(Y,matrix(0,nrow=M-N,ncol=ncol(Y)))
+  z <- c(rep(1,N),rep(0,M-N))
+  
+  ## Initial values for s
+  set.seed(932020)
+  sst <- cbind(runif(M,Xl,Xu),runif(M,Yl,Yu))
+  for(i in 1:N){
+    sst[i,1] <- mean( X[y[i,]>0,1] )
+    sst[i,2] <- mean( X[y[i,]>0,2] )
+  }
+  
+  ## NIMBLE model is nearly identical to BUGS
+  code <- nimbleCode({
+    lam0~dunif(0,5)
+    sigma~dunif(0,50) #dgamma(274.69,7.27) when informative
+    psi~dunif(0,1)
+    
+    for(i in 1:M){
+      z[i] ~ dbern(psi)
+      s[i,1] ~ dunif(Xl,Xu)
+      s[i,2] ~ dunif(Yl,Yu)
+      
+      for(j in 1:J){
+        d2[i,j] <- pow(s[i,1]-X[j,1],2) + pow(s[i,2]-X[j,2],2)
+        p[i,j] <- z[i]*lam0*exp(-(d2[i,j])/(2*sigma*sigma))
+        
+        y[i,j] ~ dpois(p[i,j]*K)
+      }#j
+    }#i
+    N <- sum(z[1:M]) #Must specify dimensions in NIMBLE
+    D <- N/A
+  })#code
+  
+  
+  # Separate data and constants (constants appear only on right-hand side of formulas)
+  nim.data <- list (y=y)
+  constants <- list (X=X, K=K, M=M, J=J, Xl=Xl, Xu=Xu, Yl=Yl, Yu=Yu, A=A)
+  
+  # Initial values (same as BUGS)
+  inits <- function(){
+    list (z=c(rep(1, N), rep(0,M-N)), psi=runif(1), sigma=runif(1,1,50), lam0=runif(1,.5,1.5), s=sst)
+  }
 
-
-# Separate data and constants (constants appear only on right-hand side of formulas)
-nim.data <- list (y=y)
-constants <- list (X=X, K=K, M=M, J=J, xlim=xlim, ylim=ylim)
-
-# Initial values (same as BUGS)
-inits <- function(){
-  list (alpha0=rnorm(1,-4,.4), alpha1=runif(1,1,2), s=sst, z=z)
+  # Parameters (same as BUGS)
+  parameters <- c("sigma","lam0","N","D")
+  
+  
+  ## Nimble steps
+  start.time <- Sys.time()  
+  Rmodel <- nimbleModel(code=code, constants=constants, data=nim.data)
+  conf <- configureMCMC(Rmodel,monitors=parameters,control = list(adaptInterval = nAdapt))
+  Rmcmc <- buildMCMC(conf)  #produces an uncompiled R mcmc function
+  Cmodel <- compileNimble(Rmodel, showCompilerOutput = TRUE)
+  Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+  samplesList <- runMCMC(Cmcmc, niter = ni, nburnin = nb, nchains = nc, inits=inits,
+                         setSeed = FALSE, progressBar = TRUE, samplesAsCodaMCMC = TRUE)
+  end.time <- Sys.time()
+  SCR0time<-end.time - start.time
+  
+  #summarize
+  summaryList<-summary(samplesList)
+  outSummary<-cbind(summaryList$statistics[,c("Mean","SD")],summaryList$quantiles[,c("2.5%","50%", "97.5%")],gelman.diag(samplesList,multivariate=FALSE)$psrf[,1],effectiveSize(samplesList))
+  colnames(outSummary)[6:7]<-c("Rhat","n.eff")
+  round(outSummary,2)
+  
+  #plot results
+  plot(samplesList[,"sigma"])
+  
 }
-
-# Parameters (same as BUGS)
-parameters <- c("alpha0","alpha1","sigma","N","D")
-
-
-## Nimble steps
-start.time <- Sys.time()  
-Rmodel <- nimbleModel(code=code, constants=constants, data=nim.data)
-conf <- configureMCMC(Rmodel,monitors=parameters,control = list(adaptInterval = nAdapt))
-Rmcmc <- buildMCMC(conf)  #produces an uncompiled R mcmc function
-Cmodel <- compileNimble(Rmodel, showCompilerOutput = TRUE)
-Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
-samplesList <- runMCMC(Cmcmc, niter = ni, nburnin = nb, nchains = nc, inits=inits,
-                       setSeed = FALSE, progressBar = TRUE, samplesAsCodaMCMC = TRUE)
-end.time <- Sys.time()
-SCR0time<-end.time - start.time
-
-#summarize
-summaryList<-summary(samplesList)
-outSummary<-cbind(summaryList$statistics[,c("Mean","SD")],summaryList$quantiles[,c("2.5%","50%", "97.5%")],gelman.diag(samplesList,multivariate=FALSE)$psrf[,1],effectiveSize(samplesList))
-colnames(outSummary)[6:7]<-c("Rhat","n.eff")
-round(outSummary,2)
-
-#plot results
-plot(samplesList[,"sigma"])
