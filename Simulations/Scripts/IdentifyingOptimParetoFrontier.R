0b5fa7d4e3b6a5673c55a0559e734481e131899a
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-03-09 18:02:33 -0800

    Script to plot pareto frontier updated with real results

--- Simulations/Scripts/IdentifyingOptimParetoFrontier.R
+++ Simulations/Scripts/IdentifyingOptimParetoFrontier.R
@@ -13,15 +13,10 @@ cost <- read_csv('Simulations/simDat/AlternateScenarioCosts.csv')
 ### Measures of performance in achieving monitoring objective.----
 
 perf <- read_csv('Simulations/Results/SimResultsSummary.csv')[,-1]
-####### ONLY FOR TESTING ##############
-perf2 <- rbind(perf,perf)
-perf2$Sim <- c("closedVISTRAP120small60half","closedTRAP60small60half","closedVIS60small60half","closedVISTRAP60small60half")
-perf <- rbind(perf,perf2)
-
 
 #### Plot initial/start-up cost comparison.----
 
-lvls <- c("VIS","TRAP Full","TRAP Half","VISTRAP Half","VISTRAP Third")
+lvls <- c("VIS", "TRAP Full","TRAP Half","VISTRAP Half","VISTRAP Third")
 
 startup <- cost %>%
   select("TotalStart","Scenario") %>%
@@ -31,10 +26,13 @@ startup <- cost %>%
   add_row(TotalStart = c("$13,700"), Scenario = c("VISTRAP Third")) %>%  ## create combined method cost
   slice(-c(1,3,5:8),) %>%  # remove separate costs of combined method scenarios
   mutate(Scenario = factor(Scenario, levels = lvls)) %>%  #reorder levels
-  mutate(Type = c("TRAP","TRAP","VIS","VISTRAP","VISTRAP"))
+  mutate(Type = c("TRAP","TRAP","VIS","VISTRAP","VISTRAP")) %>%
+  mutate(Cost = gsub(",","",TotalStart)) %>%
+  mutate(Cost = as.numeric(gsub("\\$","",Cost)))
+  
 
 
-startplot <- ggplot(startup, aes(x = Scenario, y = TotalStart, fill = Type)) +
+startplot <- ggplot(startup, aes(x = Scenario, y = Cost, fill = Type)) +
   geom_point(shape = 21, cex = 4) +
   scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
 
@@ -42,6 +40,7 @@ startplot <- ggplot(startup, aes(x = Scenario, y = TotalStart, fill = Type)) +
 
 recur <- cost %>%
   select("TotalRun","Scenario","Nights") %>%
+  filter(Nights != 60) %>%  #not including 60 days anymore
   drop_na(TotalRun) %>%
   mutate(Type = ifelse(grepl("VIS Half +|VIS Third +", Scenario),"VISTRAP",
                        ifelse(grepl("VIS ", Scenario),"VIS",
@@ -49,6 +48,7 @@ recur <- cost %>%
   mutate(Code = paste(Scenario,Nights,sep=".")) %>%
   mutate(Code = str_replace(Code, "VIS Half [+]", "VISTRAP Half")) %>%
   mutate(Code = str_replace(Code, "VIS Third [+]", "VISTRAP Third"))
+
 
 perf <- perf %>%
   mutate(Type = ifelse(grepl("VISTRAP", Sim),"VISTRAP ",
@@ -60,18 +60,38 @@ perf <- perf %>%
   mutate(Code = paste(Type,Sched,Nights,sep=""))
 
 perfcost <- inner_join(recur, perf, by = "Code")
+perfcost2 <- perfcost %>%
+  mutate(Cost = gsub(",","",TotalRun)) %>%
+  mutate(Cost = as.numeric(gsub("\\$","",Cost))) %>%
+  mutate(TypeALL = ifelse(grepl("closedTRAP",Sim),"closedTRAP",
+                          ifelse(grepl("closedVIS",Sim),"closedVIS",
+                                 ifelse(grepl("onewayTRAP",Sim),"onewayTRAP",
+                                        ifelse(grepl("onewayVIS",Sim),"onewayVIS",
+                                               ifelse(grepl("closedVISTRAP",Sim),"closedVISTRAP","onewayVISTRAP")))))) %>%
+  mutate(Truth = ifelse(grepl("120",Sim),"120","60"))
 
 ### PLOT ALL TOGETHER OR SEPARATE DIFFERENT DENSITIES AND SIZE DISTRIBUTIONS ONTO DIFFERENT PANELS?
 
 #### Plot recurring costs as compared to performance.----
 
 ## Root Mean Square Error (RMSE) - square root of the mean of the square of all error, lower value indicates more accuracy
-pareto1 <- ggplot(perfcost, aes(x = TotalRun, y = RMSE, fill = Type.x)) +
-  geom_point(shape = 21, cex = 3) +
+## For normal BTS density
+pareto120 <- ggplot(subset(perfcost2, Truth == 120), aes(x = Cost, y = RMSE, fill = TypeALL, label=Sim)) +
+  geom_point(shape = 21, cex = 5) +
   xlab("Cost in USD") + ylab("Root Mean Square Error") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
-pareto1
+  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayVIS")) +
+  geom_text(hjust=-0.1,vjust=0)
+pareto120
+
+## For low BTS density
+pareto60 <- ggplot(subset(perfcost2, Truth == 60), aes(x = Cost, y = RMSE, fill = TypeALL, label=Sim)) +
+  geom_point(shape = 21, cex = 5) +
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayVIS")) +
+  geom_text(hjust=-0.1,vjust=0)
+pareto60
 
 ## Percent coefficient of Variation (%CV) - percent of variability of posterior around the mean, lower value indicates less variability
 pareto2 <- ggplot(perfcost, aes(x = TotalRun, y = `%CV`, fill = Type.x)) +
