3e32ccb4eb0bd5393edf26873774ff0230af9bf5
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-03-09 18:02:16 -0800

    Script to look through all Rhats of results

---
+++ Simulations/Scripts/CheckRhats.R
@@ -0,0 +1,18 @@
+### Check rhats on saved results to identify simulations that need to be re-run
+
+library(jagsUI)
+
+#create a list of the files from your target directory
+file_list <- list.files(path="D:/Results-20220223-003/Results")
+
+
+for(i in 1:length(file_list)){
+    
+  load(file=paste("D:/Results-20220223-003/Results/",file_list[i],sep=""))
+    
+  print(i)
+    
+  ifelse(any(unlist(out$Rhat) > 1.1) == TRUE, stop("did not converge"), 1)
+  
+}
+  
