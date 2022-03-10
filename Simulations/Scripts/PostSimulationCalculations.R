243ae4979e46ed4a4dd1bd5aa26d92c2526bcb2f
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-03-09 18:02:47 -0800

    Calculations updated with real results

--- Simulations/Scripts/PostSimulationCalculations.R
+++ Simulations/Scripts/PostSimulationCalculations.R
@@ -7,59 +7,77 @@ rm(list=ls())
 library(HDInterval); library(dplyr); library(stringr)
 
 
-nsims <- 10#100
+nsims <- 100
 
 #### Scenario.----
 
-scen <- c("closedVIS120small60half","closedTRAP120small60half")  ## for testing
-# scen <- c("closedVIS120small60full","closedVIS120small60half","closedVIS120small60third",
-#   "closedVIS120small30full","closedVIS120small30half","closedVIS120small30third",
-#   "closedVIS120small14full","closedVIS120small14half","closedVIS120small14third",
-#   "closedVIS60small60full","closedVIS60small60half","closedVIS60small60third",
-#   "closedVIS60small30full","closedVIS60small30half","closedVIS60small30third",
-#   "closedVIS60small14full","closedVIS60small14half","closedVIS60small14third",
-#   "closedVIS120large60full","closedVIS120large60half","closedVIS120large60third",
-#   "closedVIS120large30full","closedVIS120large30half","closedVIS120large30third",
-#   "closedVIS120large14full","closedVIS120large14half","closedVIS120large14third",
-#   "closedVIS60large60full","closedVIS60large60half","closedVIS60large60third",
-#   "closedVIS60large30full","closedVIS60large30half","closedVIS60large30third",
-#   "closedVIS60large14full","closedVIS60large14half","closedVIS60large14third",
-#   "closedTRAP120small60full","closedTRAP120small60half","closedTRAP120small60third",
-#   "closedTRAP120small30full","closedTRAP120small30half","closedTRAP120small30third",
-#   "closedTRAP120small14full","closedTRAP120small14half","closedTRAP120small14third",
-#   "closedTRAP60small60full","closedTRAP60small60half","closedTRAP60small60third",
-#   "closedTRAP60small30full","closedTRAP60small30half","closedTRAP60small30third",
-#   "closedTRAP60small14full","closedTRAP60small14half","closedTRAP60small14third",
-#   "closedTRAP120large60full","closedTRAP120large60half","closedTRAP120large60third",
-#   "closedTRAP120large30full","closedTRAP120large30half","closedTRAP120large30third",
-#   "closedTRAP120large14full","closedTRAP120large14half","closedTRAP120large14third",
-#   "closedTRAP60large60full","closedTRAP60large60half","closedTRAP60large60third",
-#   "closedTRAP60large30full","closedTRAP60large30half","closedTRAP60large30third",
-#   "closedTRAP60large14full","closedTRAP60large14half","closedTRAP60large14third",
-#   "closedVISTRAP120small60halfhalf","closedVISTRAP120small60thirdthird",
+scen <- c(
+  "closedVIS120small30full","closedVIS120small30half",
+  "closedVIS120small14full","closedVIS120small14half",
+  "closedVIS60small30full","closedVIS60small30half",
+  "closedVIS60small14full","closedVIS60small14half",
+  "closedVIS120large30full","closedVIS120large30half",
+  "closedVIS120large14full","closedVIS120large14half",
+#   "closedVIS60large30full","closedVIS60large30half",
+#   "closedVIS60large14full","closedVIS60large14half",
+  "closedTRAP120small30full","closedTRAP120small30half",
+  "closedTRAP120small14full",
+# "closedTRAP120small14half",
+#   "closedTRAP60small30full","closedTRAP60small30half",
+#   "closedTRAP60small14full","closedTRAP60small14half",
+  "closedTRAP120large30full",
+# "closedTRAP120large30half",
+  "closedTRAP120large14full",
+# "closedTRAP120large14half",
+#   "closedTRAP60large30full","closedTRAP60large30half",
+#   "closedTRAP60large14full","closedTRAP60large14half",
 #   "closedVISTRAP120small30halfhalf","closedVISTRAP120small30thirdthird",
 #   "closedVISTRAP120small14halfhalf","closedVISTRAP120small14thirdthird",
-#   "closedVISTRAP60small60halfhalf","closedVISTRAP60small60thirdthird",
 #   "closedVISTRAP60small30halfhalf","closedVISTRAP60small30thirdthird",
 #   "closedVISTRAP60small14halfhalf","closedVISTRAP60small14thirdthird",
-#   "closedVISTRAP120large60halfhalf","closedVISTRAP120large60thirdthird",
 #   "closedVISTRAP120large30halfhalf","closedVISTRAP120large30thirdthird",
 #   "closedVISTRAP120large14halfhalf","closedVISTRAP120large14thirdthird",
-#   "closedVISTRAP60large60halfhalf","closedVISTRAP60large60thirdthird",
 #   "closedVISTRAP60large30halfhalf","closedVISTRAP60large30thirdthird",
-#   "closedVISTRAP60large14halfhalf","closedVISTRAP60large14thirdthird")
+#   "closedVISTRAP60large14halfhalf","closedVISTRAP60large14thirdthird",
+"onewayVIS120small30full","onewayVIS120small30half",
+"onewayVIS120small14full",
+# "onewayVIS120small14half",
+"onewayVIS60small30full","onewayVIS60small30half",
+"onewayVIS60small14full","onewayVIS60small14half",
+"onewayVIS120large30full","onewayVIS120large30half",
+"onewayVIS120large14full","onewayVIS120large14half"
+#   "onewayVIS60large30full","onewayVIS60large30half",
+#   "onewayVIS60large14full","onewayVIS60large14half",
+# "onewayTRAP120small30full","onewayTRAP120small30half",
+# "onewayTRAP120small14full","onewayTRAP120small14half",
+#   "onewayTRAP60small30full","onewayTRAP60small30half",
+#   "onewayTRAP60small14full","onewayTRAP60small14half",
+# "onewayTRAP120large30full","onewayTRAP120large30half",
+# "onewayTRAP120large14full","onewayTRAP120large14half",
+#   "onewayTRAP60large30full","onewayTRAP60large30half",
+#   "onewayTRAP60large14full","onewayTRAP60large14half",
+#   "onewayVISTRAP120small30halfhalf","onewayVISTRAP120small30thirdthird",
+#   "onewayVISTRAP120small14halfhalf","onewayVISTRAP120small14thirdthird",
+#   "onewayVISTRAP60small30halfhalf","onewayVISTRAP60small30thirdthird",
+#   "onewayVISTRAP60small14halfhalf","onewayVISTRAP60small14thirdthird",
+#   "onewayVISTRAP120large30halfhalf","onewayVISTRAP120large30thirdthird",
+#   "onewayVISTRAP120large14halfhalf","onewayVISTRAP120large14thirdthird",
+#   "onewayVISTRAP60large30halfhalf","onewayVISTRAP60large30thirdthird",
+#   "onewayVISTRAP60large14halfhalf","onewayVISTRAP60large14thirdthird"
+)
 
 type <- length(scen)
 
 #### Study area size.----
 
-for(t in 1:length(scen)){
-  if(str_detect(scen[t], "closed")){  # if study area is closed, use this area
-    A <- 52800
-  } else{                             # if study area is open, use this area
-    A <- 52800   ### UPDATE WITH FINAL OPEN AREA FROM SIMULATION
-  }
-}
+A <- 52800
+# for(t in 1:length(scen)){
+#   if(str_detect(scen[t], "closed")){  # if study area is closed, use this area
+#     A <- 52800
+#   } else{                             # if study area is open, use this area
+#     A <- 52800   ### UPDATE WITH FINAL OPEN AREA FROM SIMULATION
+#   }
+# }
 
 #### Function to find mode for continuous data.----
 
@@ -94,10 +112,10 @@ simSumm <- setNames(data.frame(matrix(ncol = 9, nrow = 0)), c("Sim","Nmn","Nmod"
 
 count <- 1  ## incremental key to store data in correct dataframe position
 
-for(t in 1:type){
-  for(i in 1:nsims){
+for(t in 17){ #1:length(scen)
+  for(i in 1:100){ #1:nsims
   
-  load(file=paste("Simulations/Results/RESULTS_",scen[t],i,".Rdata",sep=""))
+  load(file=paste("D:/Results/RESULTS_",scen[t],i,".Rdata",sep=""))
       
   print(t)
   print(i)
@@ -110,10 +128,10 @@ for(t in 1:type){
   } else{                                                         # If scenario used 60 snakes in simulation
     simSumm[count,4] <- (1/60)*(out$mean$N - 60)                  # Calculate bias
   }
-  simSumm[count,5] <- hdi(out, credMass = 0.95)[29]               # 2.5% credible interval
-  simSumm[count,6] <- hdi(out, credMass = 0.95)[30]               # 97.5% credible interval
+  simSumm[count,5] <- hdi(out, credMass = 0.95)[21]#29               # 2.5% credible interval of N
+  simSumm[count,6] <- hdi(out, credMass = 0.95)[22]#30               # 97.5% credible interval of N
   simSumm[count,7] <- out$sd$N/out$mean$N                         # Coefficient of variation
-  simSumm[count,8] <- out$Rhat$N                                  # Rhat of simulation
+  simSumm[count,8] <- out$Rhat$N                                  # Rhat for N
   simSumm[count,9] <- mean(sqrt(var(out$sims.list$N) + (out$sims.list$N - 120)^2))  # RMSE
       
   count <- count + 1
@@ -122,13 +140,16 @@ for(t in 1:type){
 }
 
 
-#### Check if any simulation didn't converge.----
+s#### Check if any simulation didn't converge.----
 for(i in 1:nrow(simSumm)){
   try(if(simSumm$Rhat[i] > 1.1) stop("did not converge"))
 }
 
 #### Write individual results to file.----
 write.csv(simSumm, file = "Simulations/Results/SimIndividualResults.csv")
+
+## remove subset for right now that didn't converge
+simSumm <- subset(simSumm, Rhat < 1.1)
 
 #### Calculate metrics.----
 
