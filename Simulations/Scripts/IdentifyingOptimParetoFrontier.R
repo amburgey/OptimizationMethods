5e9ec0024d673d1fe739e1cf8b61ba8976d6277f
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-03-30 15:13:58 -0700

    Expanding graphing to visualize all scenarios and different start-up scenarios

--- Simulations/Scripts/IdentifyingOptimParetoFrontier.R
+++ Simulations/Scripts/IdentifyingOptimParetoFrontier.R
@@ -3,7 +3,7 @@
 
 rm(list=ls())
 
-library(ggplot2);library(tidyverse);library(dplyr)
+library(ggplot2);library(tidyverse);library(dplyr);library(ggrepel)
 
 
 #### Costs of scenarios.----
@@ -19,13 +19,13 @@ perf <- read_csv('Simulations/Results/SimResultsSummary.csv')[,-1]
 lvls <- c("VIS", "TRAP Full","TRAP Half","VISTRAP Half","VISTRAP Third")
 
 startup <- cost %>%
-  select("TotalStart","Scenario") %>%
+  dplyr::select("TotalStart","Scenario") %>%
   distinct() %>%
   add_row(TotalStart = c("$2,000"), Scenario = c("VIS")) %>%
   add_row(TotalStart = c("$18,900"), Scenario = c("VISTRAP Half")) %>%  ## create combined method cost
   add_row(TotalStart = c("$13,700"), Scenario = c("VISTRAP Third")) %>%  ## create combined method cost
   slice(-c(1,3,5:8),) %>%  # remove separate costs of combined method scenarios
-  mutate(Scenario = factor(Scenario, levels = lvls)) %>%  #reorder levels
+  mutate(Code2 = factor(Scenario, levels = lvls)) %>%  #reorder levels
   mutate(Type = c("TRAP","TRAP","VIS","VISTRAP","VISTRAP")) %>%
   mutate(Cost = gsub(",","",TotalStart)) %>%
   mutate(Cost = as.numeric(gsub("\\$","",Cost)))
@@ -34,12 +34,13 @@ startup <- cost %>%
 
 startplot <- ggplot(startup, aes(x = Scenario, y = Cost, fill = Type)) +
   geom_point(shape = 21, cex = 4) +
+  ylab("Startup Cost") + xlab("Monitoring Scenario") +
   scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
 
 #### Combine performance and cost datasets to figure out value for estimate achieved.----
 
 recur <- cost %>%
-  select("TotalRun","Scenario","Nights") %>%
+  dplyr::select("TotalRun","Scenario","Nights") %>%
   filter(Nights != 60) %>%  #not including 60 days anymore
   drop_na(TotalRun) %>%
   mutate(Type = ifelse(grepl("VIS Half +|VIS Third +", Scenario),"VISTRAP",
@@ -49,7 +50,6 @@ recur <- cost %>%
   mutate(Code = str_replace(Code, "VIS Half [+]", "VISTRAP Half")) %>%
   mutate(Code = str_replace(Code, "VIS Third [+]", "VISTRAP Third"))
 
-
 perf <- perf %>%
   mutate(Type = ifelse(grepl("VISTRAP", Sim),"VISTRAP ",
                        ifelse(grepl("TRAP",Sim),"TRAP ","VIS "))) %>%
@@ -57,7 +57,10 @@ perf <- perf %>%
                        ifelse(grepl("half",Sim),"Half.","Third."))) %>%
   mutate(Nights = ifelse(grepl("l60|e60", Sim),"60",
                          ifelse(grepl("l30|e30",Sim),"30","14"))) %>%
-  mutate(Code = paste(Type,Sched,Nights,sep=""))
+  mutate(Code = paste(Type,Sched,Nights,sep="")) %>%
+  mutate(Code2 = paste(Type,gsub("\\.","",Sched),sep="")) %>%
+  mutate(Code2 = ifelse(grepl("VIS Full",Code2),"VIS",
+                        ifelse(grepl("VIS Half",Code2),"VIS",Code2)))
 
 perfcost <- inner_join(recur, perf, by = "Code")
 perfcost2 <- perfcost %>%
@@ -68,52 +71,146 @@ perfcost2 <- perfcost %>%
                                  ifelse(grepl("onewayTRAP",Sim),"onewayTRAP",
                                         ifelse(grepl("onewayVIS",Sim),"onewayVIS",
                                                ifelse(grepl("closedVISTRAP",Sim),"closedVISTRAP","onewayVISTRAP")))))) %>%
-  mutate(Truth = ifelse(grepl("120",Sim),"120","60"))
+  mutate(Truth = ifelse(grepl("120",Sim),"120","60")) %>%
+  mutate(Scenario = sub(".* ","",Code)) %>%
+  mutate(Size = ifelse(grepl("large",Sim),"large",
+                          ifelse(grepl("small",Sim),"small",NA)))
 
-### PLOT ALL TOGETHER OR SEPARATE DIFFERENT DENSITIES AND SIZE DISTRIBUTIONS ONTO DIFFERENT PANELS?
 
 #### Plot recurring costs as compared to performance.----
 
 ## Root Mean Square Error (RMSE) - square root of the mean of the square of all error, lower value indicates more accuracy
 ## For normal BTS density
-pareto120 <- ggplot(subset(perfcost2, Truth == 120), aes(x = Cost, y = RMSE, fill = TypeALL, label=Sim)) +
-  geom_point(shape = 21, cex = 5) +
+pareto120 <- ggplot(subset(perfcost2, Truth == 120), aes(x = Cost, y = RMSE, fill = TypeALL, shape = Scenario, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) + 
   xlab("Cost in USD") + ylab("Root Mean Square Error") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayVIS")) +
-  geom_text(hjust=-0.1,vjust=0)
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1)) +
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 120 & Code == c("VIS Half.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
 pareto120
 
 ## For low BTS density
-pareto60 <- ggplot(subset(perfcost2, Truth == 60), aes(x = Cost, y = RMSE, fill = TypeALL, label=Sim)) +
-  geom_point(shape = 21, cex = 5) +
+pareto60 <- ggplot(subset(perfcost2, Truth == 60), aes(x = Cost, y = RMSE, fill = TypeALL, shape = Scenario, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) +
   xlab("Cost in USD") + ylab("Root Mean Square Error") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayVIS")) +
-  geom_text(hjust=-0.1,vjust=0)
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1), check_overlap = T)
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 60 & Code == c("VIS Full.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
 pareto60
 
-## Percent coefficient of Variation (%CV) - percent of variability of posterior around the mean, lower value indicates less variability
-pareto2 <- ggplot(perfcost, aes(x = TotalRun, y = `%CV`, fill = Type.x)) +
-  geom_point(shape = 21, cex = 3) +
-  xlab("Cost in USD") + ylab("% Coefficient of Variation") +
-  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
-pareto2
 
-## Percent bias (%Bias) - each estimate subtracted from the true abundance, value closer to zero indicates less bias
-pareto3 <- ggplot(perfcost, aes(x = TotalRun, y = `%Bias`, fill = Type.x)) +
-  geom_point(shape = 21, cex = 3) +
-  xlab("Cost in USD") + ylab("% Bias") +
-  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
-pareto3
 
-## Percent coverage (%Coverage) - percent of simulations where the highest density posterior interval (HDPI) included the true abundance, closer to 100% indicates better coverage
-pareto4 <- ggplot(perfcost, aes(x = TotalRun, y = `%Coverage`, fill = Type.x)) +
-  geom_point(shape = 21, cex = 3) +
-  xlab("Cost in USD") + ylab("% Coverage") +
+#### Plot start-up cost scenarios plus recurring costs as compared to performance.----
+
+## Combine start-up and recurring costs
+fullcost <- merge(startup, perfcost2, by = c("Code2"))
+fullcost$TotalCost <- fullcost$Cost.x + fullcost$Cost.y
+
+## Scenario One: Full start-up costs (aka, no equipment already purchased) plus recurring costs
+
+## Root Mean Square Error (RMSE) - square root of the mean of the square of all error, lower value indicates more accuracy
+## For normal BTS density
+pareto120FULL <- ggplot(subset(fullcost, Truth == 120), aes(x = TotalCost, y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) + 
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
   theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
-  scale_fill_manual(values = c("#799D31","#B77A29","#FDEC9E"), breaks = c("TRAP","VIS","VISTRAP"))
-pareto4
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1)) +
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 120 & Code == c("VIS Half.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto120FULL
+
+## For low BTS density
+pareto60FULL <- ggplot(subset(fullcost, Truth == 60), aes(x = TotalCost, y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) +
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1), check_overlap = T)
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 60 & Code == c("VIS Full.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto60FULL
+
+## Scenario Two: Half start-up costs (aka, some equipment/replacements needed) plus recurring costs
+
+## Root Mean Square Error (RMSE) - square root of the mean of the square of all error, lower value indicates more accuracy
+## For normal BTS density
+pareto120HALF <- ggplot(subset(fullcost, Truth == 120), aes(x = (TotalCost-(Cost.x/2)), y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) + 
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1)) +
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 120 & Code == c("VIS Half.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto120HALF
+
+## For low BTS density
+pareto60HALF <- ggplot(subset(fullcost, Truth == 60), aes(x = (TotalCost-(Cost.x/2)), y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) +
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1), check_overlap = T)
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 60 & Code == c("VIS Full.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto60HALF
+
+## Scenario Three: No start-up costs (aka, all equipment purchased already) plus recurring costs (SAME AS FIRST PARETO GRAPHS)
+
+## Root Mean Square Error (RMSE) - square root of the mean of the square of all error, lower value indicates more accuracy
+## For normal BTS density
+pareto120NONE <- ggplot(subset(fullcost, Truth == 120), aes(x = (TotalCost-Cost.x), y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) + 
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1)) +
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 120 & Code == c("VIS Half.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto120NONE
+
+## For low BTS density
+pareto60NONE <- ggplot(subset(fullcost, Truth == 60), aes(x = (TotalCost-Cost.x), y = RMSE, fill = TypeALL, shape = Scenario.y, size = as.factor(Size))) + #, label=Sim
+  geom_jitter(alpha = 0.75) +
+  xlab("Cost in USD") + ylab("Root Mean Square Error") +
+  theme(axis.title = element_text(size = 12), axis.text = element_text(size = 10)) +
+  scale_fill_manual(values = c("#799D31","#B77A29","#C9F277","#FDEC9E"), breaks = c("closedTRAP","closedVIS","onewayTRAP","onewayVIS")) +
+  # geom_text(position = position_jitter(width = 1, height = 1), check_overlap = T)
+  # geom_text(
+  # data=perfcost2 %>% filter(Truth == 60 & Code == c("VIS Full.14")), # Filter data first
+  # aes(label=Sim), position = position_jitter(width = -2, height = 3)) +
+  scale_shape_manual(values = c(21, 22, 24, 25)) +
+  scale_size_manual(values=c(3.5,5))
+pareto60NONE
+
+
+
+
+
 
