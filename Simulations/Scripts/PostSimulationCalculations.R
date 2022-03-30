0fbbdc94cb8d324020903ef20573b911a1799926
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-03-30 15:14:46 -0700

    Update to make calculation dynamic depending on what sims actually converged, skip missing (failed) ones

--- Simulations/Scripts/PostSimulationCalculations.R
+++ Simulations/Scripts/PostSimulationCalculations.R
@@ -18,19 +18,16 @@ scen <- c(
   "closedVIS60small14full","closedVIS60small14half",
   "closedVIS120large30full","closedVIS120large30half",
   "closedVIS120large14full","closedVIS120large14half",
-#   "closedVIS60large30full","closedVIS60large30half",
-#   "closedVIS60large14full","closedVIS60large14half",
+  "closedVIS60large30full","closedVIS60large30half",
+  "closedVIS60large14full","closedVIS60large14half",
   "closedTRAP120small30full","closedTRAP120small30half",
-  "closedTRAP120small14full",
-# "closedTRAP120small14half",
-#   "closedTRAP60small30full","closedTRAP60small30half",
-#   "closedTRAP60small14full","closedTRAP60small14half",
-  "closedTRAP120large30full",
-# "closedTRAP120large30half",
-  "closedTRAP120large14full",
-# "closedTRAP120large14half",
-#   "closedTRAP60large30full","closedTRAP60large30half",
-#   "closedTRAP60large14full","closedTRAP60large14half",
+  "closedTRAP120small14full","closedTRAP120small14half",
+  "closedTRAP60small30full","closedTRAP60small30half",
+  "closedTRAP60small14full","closedTRAP60small14half",
+  "closedTRAP120large30full","closedTRAP120large30half",
+  "closedTRAP120large14full","closedTRAP120large14half",
+  "closedTRAP60large30full","closedTRAP60large30half",
+  "closedTRAP60large14full","closedTRAP60large14half",
 #   "closedVISTRAP120small30halfhalf","closedVISTRAP120small30thirdthird",
 #   "closedVISTRAP120small14halfhalf","closedVISTRAP120small14thirdthird",
 #   "closedVISTRAP60small30halfhalf","closedVISTRAP60small30thirdthird",
@@ -39,23 +36,22 @@ scen <- c(
 #   "closedVISTRAP120large14halfhalf","closedVISTRAP120large14thirdthird",
 #   "closedVISTRAP60large30halfhalf","closedVISTRAP60large30thirdthird",
 #   "closedVISTRAP60large14halfhalf","closedVISTRAP60large14thirdthird",
-"onewayVIS120small30full","onewayVIS120small30half",
-"onewayVIS120small14full",
-# "onewayVIS120small14half",
-"onewayVIS60small30full","onewayVIS60small30half",
-"onewayVIS60small14full","onewayVIS60small14half",
-"onewayVIS120large30full","onewayVIS120large30half",
-"onewayVIS120large14full","onewayVIS120large14half"
-#   "onewayVIS60large30full","onewayVIS60large30half",
-#   "onewayVIS60large14full","onewayVIS60large14half",
-# "onewayTRAP120small30full","onewayTRAP120small30half",
-# "onewayTRAP120small14full","onewayTRAP120small14half",
-#   "onewayTRAP60small30full","onewayTRAP60small30half",
-#   "onewayTRAP60small14full","onewayTRAP60small14half",
-# "onewayTRAP120large30full","onewayTRAP120large30half",
-# "onewayTRAP120large14full","onewayTRAP120large14half",
-#   "onewayTRAP60large30full","onewayTRAP60large30half",
-#   "onewayTRAP60large14full","onewayTRAP60large14half",
+  "onewayVIS120small30full","onewayVIS120small30half",
+  "onewayVIS120small14full","onewayVIS120small14half",
+  "onewayVIS60small30full","onewayVIS60small30half",
+  "onewayVIS60small14full","onewayVIS60small14half",
+  "onewayVIS120large30full","onewayVIS120large30half",
+  "onewayVIS120large14full","onewayVIS120large14half",
+  "onewayVIS60large30full","onewayVIS60large30half",
+  "onewayVIS60large14full","onewayVIS60large14half",
+  "onewayTRAP120small30full","onewayTRAP120small30half",
+  "onewayTRAP120small14full","onewayTRAP120small14half",
+  "onewayTRAP60small30full","onewayTRAP60small30half",
+  "onewayTRAP60small14full","onewayTRAP60small14half",
+  "onewayTRAP120large30full","onewayTRAP120large30half",
+  "onewayTRAP120large14full","onewayTRAP120large14half",
+  "onewayTRAP60large30full","onewayTRAP60large30half",
+  "onewayTRAP60large14full","onewayTRAP60large14half"
 #   "onewayVISTRAP120small30halfhalf","onewayVISTRAP120small30thirdthird",
 #   "onewayVISTRAP120small14halfhalf","onewayVISTRAP120small14thirdthird",
 #   "onewayVISTRAP60small30halfhalf","onewayVISTRAP60small30thirdthird",
@@ -110,32 +106,42 @@ simSumm <- setNames(data.frame(matrix(ncol = 9, nrow = 0)), c("Sim","Nmn","Nmod"
 
 #### Pull info from model results.----
 
+listfiles <- list.files("D:/Results/")
+
 count <- 1  ## incremental key to store data in correct dataframe position
 
-for(t in 17){ #1:length(scen)
-  for(i in 1:100){ #1:nsims
-  
+for(t in 1:length(scen)){ #1:length(scen)
+  for(i in 1:nsims){ #1:nsims, length(list.files("D:/Results/", pattern = scen[t]))
+
+  if(file.exists(file.path(paste("D:/Results/RESULTS_",scen[t],i,".Rdata",sep=""))) == FALSE)
+  {
+    next
+  }
   load(file=paste("D:/Results/RESULTS_",scen[t],i,".Rdata",sep=""))
-      
+
   print(t)
   print(i)
-      
+
   simSumm[count,1] <- scen[t]                                     # Scenario
   simSumm[count,2] <- out$mean$N                                  # Mean Abundance
   simSumm[count,3] <- mode(out$sims.list$N)                       # Mode Abundance
   if(str_detect(scen[t], "120")){                                 # If scenario used 120 snakes in simulation
     simSumm[count,4] <- (1/120)*(out$mean$N - 120)                # Calculate bias
-  } else{                                                         # If scenario used 60 snakes in simulation
+  } else {                                                         # If scenario used 60 snakes in simulation
     simSumm[count,4] <- (1/60)*(out$mean$N - 60)                  # Calculate bias
   }
   simSumm[count,5] <- hdi(out, credMass = 0.95)[,"N"][1]          # 2.5% credible interval of N
   simSumm[count,6] <- hdi(out, credMass = 0.95)[,"N"][2]          # 97.5% credible interval of N
   simSumm[count,7] <- out$sd$N/out$mean$N                         # Coefficient of variation
   simSumm[count,8] <- out$Rhat$N                                  # Rhat for N
-  simSumm[count,9] <- mean(sqrt(var(out$sims.list$N) + (out$sims.list$N - 120)^2))  # RMSE
-      
+  if(str_detect(scen[t], "120")){
+    simSumm[count,9] <- mean(sqrt(var(out$sims.list$N) + (out$sims.list$N - 120)^2))  # RMSE for 120 snakes in simulation
+  } else {
+    simSumm[count,9] <- mean(sqrt(var(out$sims.list$N) + (out$sims.list$N - 60)^2))  # RMSE for 60 snakes in simulation
+  }
+
   count <- count + 1
-      
+
   }
 }
 
@@ -148,9 +154,6 @@ for(i in 1:nrow(simSumm)){
 #### Write individual results to file.----
 write.csv(simSumm, file = "Simulations/Results/SimIndividualResults.csv")
 
-############ TEMPORARY #################
-## remove subset for right now that didn't converge
-simSumm <- subset(simSumm, Rhat < 1.1)
 
 #### Calculate metrics.----
 
@@ -176,21 +179,6 @@ snakecov <- aggregate(simSumm$Cov, list(simSumm$Sim), sum)
 snakecov <- snakecov[order(snakecov[,1]),]
 
 ## Calculate RMSE of snake abundance by each monitoring scenario
-# mse <- as.data.frame(matrix(NA, nrow = length(scen), ncol = 2, dimnames = list(seq(1:length(scen)),c("Group.1","x"))))
-# rmse <- as.data.frame(matrix(NA, nrow = length(scen), ncol = 2, dimnames = list(seq(1:length(scen)),c("Group.1","x"))))
-# for(t in 1:nrow(simSumm)){
-#   if(str_detect(simSumm$Sim[t], "120")){                                  # If scenario used 120 snakes in simulation
-#     mse[t,1] <- simSumm$Sim[t]                                            # Scenario type
-#     mse[t,2] <- apply((as.data.frame(snakenums[t,2]) - 120)^2 ,1,mean)    # mean squared error
-#     rmse[t,1] <- simSumm$Sim[t]                                           # Scenario type
-#     rmse[t,2] <- sqrt(mse[t,2])                                           # root mean squared error
-#   } else{                                                                 # If scenario used 60 snakes in simulation
-#     mse[t,1] <- simSumm$Sim[t]                                            # Scenario type
-#     mse[t,2] <- apply((as.data.frame(snakenums[t,2]) - 60)^2 ,1,mean)     # mean squared error
-#     rmse[t,1] <- simSumm$Sim[t]                                           # Scenario type
-#     rmse[t,2] <- sqrt(mse[t,2])                                           # root mean squared error
-#   }
-# }
 snakermse <- aggregate(simSumm$RMSE, list(simSumm$Sim), mean)
 snakermse <- snakermse[order(snakermse[,1]),]
 
@@ -209,3 +197,4 @@ all <- cbind(snakenums,snakemod[,2],snakecov[,2],snakeCV[,2],snakebias[,2],snake
 colnames(all) <- c("Sim","Nmn","Nmod","%Coverage","%CV","%Bias","RMSE")
 
 write.csv(all,"Simulations/Results/SimResultsSummary.csv")
+
