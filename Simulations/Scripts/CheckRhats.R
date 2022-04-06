afb793c523fbbb00e913e9031b40df447149253d
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-04-06 11:00:42 -0700

    Adjust rhat so rounding to less than 1.1

--- Simulations/Scripts/CheckRhats.R
+++ Simulations/Scripts/CheckRhats.R
@@ -7,12 +7,12 @@ file_list <- list.files(path="D:/Results")
 
 
 for(i in 1:length(file_list)){
-    
+  
   load(file=paste("D:/Results/",file_list[i],sep=""))
     
   print(i)
     
-  ifelse(any(unlist(out$Rhat) > 1.1) == TRUE, stop("did not converge"), 1)
+  ifelse(any(unlist(out$Rhat) > 1.1095) == TRUE, stop("did not converge"), 1)
   
 }
   
