7f50f7546e331ad7b921fa05d865402f73d7ae1d
Staci Amburgey <staci.m.amburgey@gmail.com>
2022-06-01 15:20:24 -0700

    Clarify purpose of rhat check

--- Simulations/Scripts/CheckRhats.R
+++ Simulations/Scripts/CheckRhats.R
@@ -1,14 +1,16 @@
-### Check rhats on saved results to identify simulations that need to be re-run
+### Script to check rhats of saved results to identify simulations that need to be re-run
+### list.files will run through all files in the specified directory
+### Script will stop and throw error when it finds a failed simulation - have to manually restart after removing the failed simulation
 
 library(jagsUI)
 
 #create a list of the files from your target directory
-file_list <- list.files(path="D:/Results")
+file_list <- list.files(path="D:/Simulations/Results")
 
 
 for(i in 1:length(file_list)){
   
-  load(file=paste("D:/Results/",file_list[i],sep=""))
+  load(file=paste("D:/Simulations/Results/",file_list[i],sep=""))
     
   print(i)
     
